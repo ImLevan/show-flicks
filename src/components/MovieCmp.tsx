b7e1b26428a62d24d4f0ea8ecaaf0dd3bd06c95a
ImLevan <chianesevalentin@gmail.com>
2024-02-11 11:11:26 -0300

    change row font size

--- src/components/MovieCmp.tsx
+++ src/components/MovieCmp.tsx
@@ -19,7 +19,7 @@ const MovieCmp: React.FC<Props> = ({movie}) => {
             <div className="w-[60%] sm:w-[50%] md:w-[40%] lg:w-[30%] xl:w-[25%] inline-block cursor-pointer relative p-2">
                 <img className="w-full rounded-lg h-auto block" src={`https://image.tmdb.org/t/p/w500/${movie?.backdrop_path}`} alt={movie?.title} />
                 <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-                    <p className="white-space-normal text-sm md:text-base lg:text-sm xl:text-base 2xl:text-xl font-bold flex justify-center items-center h-full text-center">
+                    <p className="white-space-normal text-xs md:xs lg:text-xs xl:text-sm font-bold flex justify-center items-center h-full text-center">
                         {movie?.title}<br></br>
                         {movie?.release_date ? monthNames[new Date(movie.release_date).getMonth()] + " " + new Date(movie.release_date).getFullYear(): null}
                     </p>
